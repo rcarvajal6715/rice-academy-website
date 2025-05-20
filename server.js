5ca350efe9f91a4475f5247a6a968dff9cc5feff
rcarvajal6715 <carvajalinoricardo0505@gmail.com>
2025-05-19 22:45:24 -0500

    Initial commit - C2 Tennis Academy backend and frontend

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 
 const pool = new Pool({

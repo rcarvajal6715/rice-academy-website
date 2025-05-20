0bf87264df4bd6060bf636413d7bdbb3cae24066
rcarvajal6715 <carvajalinoricardo0505@gmail.com>
2025-05-19 22:41:14 -0500

    Add homepage route to fix Cannot GET /

--- server.js
+++ server.js
@@ -322,6 +322,10 @@ async function sendInvoiceEmail(toEmail, session) {
   doc.end();
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);

086f6d6399201d95b1fcd91d086f2be5b450ed35
rcarvajal6715 <carvajalinoricardo0505@gmail.com>
2025-06-02 20:03:32 -0500

    Add latest changes

--- server.js
+++ server.js
@@ -883,4 +883,4 @@ app.get('/:page', (req, res) => {
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
-});
+});  jhfh
